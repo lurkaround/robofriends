58e61c510847c509d99511433ec8bdd688fd8f10
Helon <rhelon@gmail.com>
2019-07-24 17:36:24 +0200

    Updates

---
+++ precache-manifest.011c12c5f29bf807746cccccb615effe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45cb17f7f865bb6447e6762ede43c2b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b723a77633d34b911959",
+    "url": "/robofriends/static/css/main.b53edabc.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "b723a77633d34b911959",
+    "url": "/robofriends/static/js/main.c68a5947.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
